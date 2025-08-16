3ebf13d1e49015d1852465897ef6ff0943e88209
Kok321416xxx <kok321416x@yandex.ru>
2025-08-16 11:41:10 +0800

    домашняя работа 26_1

--- catalog/admin.py
+++ catalog/admin.py
@@ -1,3 +1,26 @@
 from django.contrib import admin
+from .models import Product
 
-# Register your models here.
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    """
+    Административная панель для модели Product
+    """
+    list_display = ['name', 'price', 'is_active', 'created_at']
+    list_filter = ['is_active', 'created_at']
+    search_fields = ['name', 'description']
+    list_editable = ['is_active']
+    readonly_fields = ['created_at', 'updated_at']
+    
+    fieldsets = (
+        ('Основная информация', {
+            'fields': ('name', 'description', 'price')
+        }),
+        ('Статус', {
+            'fields': ('is_active',)
+        }),
+        ('Даты', {
+            'fields': ('created_at', 'updated_at'),
+            'classes': ('collapse',)
+        }),
+    )
